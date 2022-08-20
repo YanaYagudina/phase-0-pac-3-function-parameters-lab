72bb7743a36875ae140e94b308bc1f34d109de9d
Yana <yana.videoeditor.gmail.com>
2022-08-19 19:15:57 -0600

    commit tree ddcceb297a0f01bd9e826be4cd8ac44f0b02e541

---
+++ index.js
@@ -0,0 +1,17 @@
+/* function introductionWithLanguage(name, language) {
+    console.log('Hi, my name is ${name} and I am learning to program in ${language}')
+}
+introductionWithLanguage("Yana", "JS") 
+*/
+
+function introduction(name) {
+return `Hi, my name is ${name}.`
+}
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
